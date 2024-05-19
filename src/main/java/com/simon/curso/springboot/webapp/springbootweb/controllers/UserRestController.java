28de2dd7546c1a716fdb4935183de7861f96fb04
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2024-05-19 19:35:05 -0400

    Clase DTO

--- src/main/java/com/simon/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/simon/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simon.curso.springboot.webapp.springbootweb.models.User;
+import com.simon.curso.springboot.webapp.springbootweb.models.dto.UserDto;
 
 
 //Importando este decorador, puedo transformar una clase para que contenga APIs Rest
@@ -18,8 +19,8 @@ public class UserRestController {
     //GetMapping es una abreviatura de RequestMapping, 
     //ya que el valor por defecto del RequestMapping es el GetMapping
     //@GetMapping("/details-rest") -> @RequestMapping(path="/details-rest", method=RequestMethod.GET)
-    @GetMapping("/details-rest")
-    public Map<String, Object> details(){
+    @GetMapping("/details-rest-map")
+    public Map<String, Object> detailsMap(){
 
         Map<String, Object> body = new HashMap<>();
         User user = new User("Simon", "Bustamante");
@@ -28,6 +29,19 @@ public class UserRestController {
         body.put("user", user);
 
         return body;
+    }
+
+    @GetMapping("/details-rest-dto")
+    public UserDto detailsDto(){
+
+        //TODO Usar una clase POJO para poder almacenar la data a mostrar en el APIrest
+        UserDto userDto = new UserDto();
+        User user = new User("Simon", "Bustamante");
+
+        userDto.setUser(user);
+        userDto.setTitle("Hola mundo Spring Boot Dto");
+
+        return userDto;
     }
 
 
---
+++ src/main/java/com/simon/curso/springboot/webapp/springbootweb/models/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.simon.curso.springboot.webapp.springbootweb.models.dto;
+
+import com.simon.curso.springboot.webapp.springbootweb.models.User;
+
+public class UserDto {
+
+    private String title;
+    private User user; //Objeto anidado
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+}
