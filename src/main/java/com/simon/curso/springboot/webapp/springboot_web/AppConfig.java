0e811a88cddf7dc97c42a0609116a30328f1578b
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-06-28 17:57:34 -0400

    avance cap 63 anotacion bean

--- src/main/java/com/simon/curso/springboot/webapp/springboot_web/AppConfig.java
+++ src/main/java/com/simon/curso/springboot/webapp/springboot_web/AppConfig.java
@@ -1,10 +1,19 @@
 package com.simon.curso.springboot.webapp.springboot_web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.simon.curso.springboot.webapp.springboot_web.controllers.ProductRepositoryJson;
+import com.simon.curso.springboot.webapp.springboot_web.repositories.ProductRepository;
 
 @Configuration
 @PropertySource("classpath:config.properties")
 public class AppConfig {
+
+    @Bean
+    ProductRepository productRepositoryJson() {
+        return new ProductRepositoryJson();
+    }
 
 }
--- src/main/java/com/simon/curso/springboot/webapp/springboot_web/controllers/ProductRepositoryJson.java
+++ src/main/java/com/simon/curso/springboot/webapp/springboot_web/controllers/ProductRepositoryJson.java
@@ -32,7 +32,9 @@ public class ProductRepositoryJson  implements ProductRepository {
 
     @Override
     public Product findById(Long id) {
-        return null;
+        return list.stream()
+        .filter(p -> p.getId().equals(id) )
+        .findFirst().orElseThrow();
     }
 
 }
